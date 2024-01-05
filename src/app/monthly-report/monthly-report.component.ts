cc6a09da4887d1bc904d033615375dfac5fdd596
Sherif Hamdy <sherif_hfm@yahoo.com>
2024-01-05 21:00:10 +0300

    updates

--- src/app/monthly-report/monthly-report.component.ts
+++ src/app/monthly-report/monthly-report.component.ts
@@ -3,10 +3,6 @@ import { AuthService} from '../services/auth.service';
 import { ArabicNumsService} from '../services/arabic-nums.service';
 import { ReportsService} from '../services/reports.service';
 
-import * as pdfMake from "pdfmake/build/pdfmake";
-import * as pdfFonts from 'pdfmake/build/vfs_fonts';
-(pdfMake as any).vfs = pdfFonts.pdfMake.vfs;
-
 @Component({
   selector: 'app-monthly-report',
   templateUrl: './monthly-report.component.html',
@@ -18,7 +14,7 @@ export class MonthlyReportComponent implements OnInit {
   funeralList:any[]=[];
   
   constructor(public reportsService: ReportsService,public arabicNums: ArabicNumsService) {
-    //(pdfMake as any).vfs = pdfFonts.pdfMake.vfs;
+
    }
 
   ngOnInit(): void {
@@ -38,28 +34,5 @@ export class MonthlyReportComponent implements OnInit {
   }
   search(){
     this.getMonthlyReport();
-  }
-
-  download(){
-    console.log(pdfMake.vfs);
-    
-    const documentDefinition = {
-      content: [
-        'This is an example PDF generated from Angular component-rendered HTML!',
-        {
-          text: 'السلام عليكم',
-          style: 'header'
-        },
-        // Add more content as needed...
-      ],
-      styles: {
-        header: {
-          fontSize: 18,
-          font: 'Roboto'
-        }
-      }
-    };
-  
-    pdfMake.createPdf(documentDefinition).download('example.pdf');
   }
 }
